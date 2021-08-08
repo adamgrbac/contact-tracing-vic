6080d21ec1d37e0831b4ef6574dde1f79f5d62fe
Adam Grbac <adam.grbac@gmail.com>
2021-08-08 20:23:17 +1000

    Fixed and formatted

--- contact-tracing-vic.py
+++ contact-tracing-vic.py
@@ -1,22 +1,59 @@
-import requests
+import pandas as pd
+import sqlite3
+import yagmail
+import utils
+import yaml
+from datetime import datetime
 
-# Vic notes:
+# Load email config
+with open("email_config.yml", "r") as f:
+    email_config = yaml.safe_load(f)
 
-ckanAPIKey = "f6325461-8ec7-4b06-99b3-cd64c014cca8"
-ckanResourceID = "79adc77f-6ea7-43f9-8af0-167da76456ad"
-ckanBaseURL = "https://discover.data.vic.gov.au/api/3/action/datastore_search"
-api_base_url = "https://discover.data.vic.gov.au"
+# Setup Email details
+yag = yagmail.SMTP(email_config["sender"], oauth2_file="oauth2_file.json")
 
-headers = {"authorization": ckanAPIKey}
+# Open DB Connection
+con = sqlite3.connect("contact_tracing_vic.db")
+cur = con.cursor()
 
-res = requests.get(ckanBaseURL+"?resource_id="+ckanResourceID, headers=headers)
+# Prep database tables
+utils.prep_database(con)
 
-res_dicts = []
+# Get time of latest update & create table if missing
+max_time = [row for row in cur.execute("SELECT max(data_added) FROM contact_tracing")][0][0]
+if not max_time:
+    max_time = datetime(1900, 1, 1, 0, 0, 0)
 
-while res.status_code == 200 and len(res.json()["result"]["records"]) > 0:
-	print("Records found! Appending to list...")
-	res_dicts.append(res.json())
-	print("Looking for more records...")
-	res = requests.get(api_base_url+res.json()["result"]["_links"]["next"], headers=headers)
-		
-print(res_dicts)
+# Get Vic Data
+res_dicts = utils.get_data()
+
+df = pd.DataFrame(res_dicts)
+df = utils.clean_dataframe(df)
+
+# Get records that have appeared since last entry in the database
+# Separate these rows into new rows and updated rows based on updated_flag
+new_records = df[df["data_added"] > max_time]
+
+# If there are any new / updated rows, process and email to dist list
+if len(new_records) > 0:
+
+    # Email body
+    contents = []
+
+    # Create upto two sections depending on presences of new/updated records
+    if len(new_records) > 0:
+        contents.append("New / Updated Contact Tracing Locations added to the website:")
+        contents.append(utils.htmlify(new_records))
+
+    # Send email to dist list
+    yag.send(bcc=email_config["dist_list"], subject="New Contact Tracing Locations!", contents=contents)
+
+    # Insert new records into database to mark them as processed
+    new_records.to_sql("contact_tracing", con, if_exists="append", index=False)
+
+else:
+    # For logging purposes
+    print("No updates!")
+
+# Close DB connection
+con.close()
--- utils.py
+++ utils.py
@@ -1,5 +1,46 @@
 import pandas as pd
-from urllib.parse import unquote
+import sqlite3
+import requests
+
+
+def get_data() -> list:
+
+    ckanAPIKey = "f6325461-8ec7-4b06-99b3-cd64c014cca8"
+    ckanResourceID = "afb52611-6061-4a2b-9110-74c920bede77"
+    ckanBaseURL = "https://discover.data.vic.gov.au/api/3/action/datastore_search"
+    api_base_url = "https://discover.data.vic.gov.au"
+
+    headers = {"authorization": ckanAPIKey}
+
+    res = requests.get(ckanBaseURL+"?resource_id="+ckanResourceID, headers=headers)
+
+    res_dicts = []
+
+    while res.status_code == 200 and len(res.json()["result"]["records"]) > 0:
+        res_dicts += res.json()["result"]["records"]
+        res = requests.get(api_base_url+res.json()["result"]["_links"]["next"], headers=headers)
+
+    return res_dicts
+
+
+def prep_database(con: sqlite3.Connection) -> None:
+
+    cur = con.cursor()
+
+    # Create history table if missing
+    cur.execute("""
+        CREATE TABLE IF NOT EXISTS contact_tracing (
+           severity varchar(256),
+           data_date timestamp,
+           data_location varchar(256),
+           data_address varchar(256),
+           data_suburb varchar(256),
+           data_datetext varchar(256),
+           data_timetext varchar(256),
+           data_added  timestamp
+        );""")
+
+    cur.close()
 
 
 def htmlify(df: pd.DataFrame) -> str:
@@ -20,7 +61,7 @@ def htmlify(df: pd.DataFrame) -> str:
     return output
 
 
-def clean_dataframe(df: pd.DataFrame, table_name: str) -> pd.DataFrame:
+def clean_dataframe(df: pd.DataFrame) -> pd.DataFrame:
     """
     Description:
         clean_dataframe cleans a pandas DataFrame by correcting formatting
@@ -34,16 +75,17 @@ def clean_dataframe(df: pd.DataFrame, table_name: str) -> pd.DataFrame:
 
     col_names = list(df.columns)
 
-    df["severity"] = table_name
-    df["data_date"] = pd.to_datetime(df["data-date"])
-    df["data_location"] = df["data-location"].apply(unquote)
-    df["data_address"] = df["data-address"].apply(unquote)
-    df["data_suburb"] = df["data-suburb"].apply(unquote)
-    df["data_datetext"] = df["data-datetext"].apply(unquote)
-    df["data_timetext"] = df["data-timetext"].apply(unquote)
-    df["data_added"] = pd.to_datetime(df["data-added"])
-    df["updated_flag"] = False if "class" not in df.columns else df["class"].apply(lambda x: False if type(x) != list else "qh-updated" in x)
+    df["severity"] = df["Advice_title"].apply(lambda x:  x[:6])
+    df["data_date"] = pd.to_datetime(df["Exposure_date"], format="%d/%m/%Y")
+    df["data_location"] = df["Site_title"]
+    df["data_address"] = df["Site_streetaddress"]
+    df["data_suburb"] = df["Suburb"]
+    df["data_datetext"] = pd.to_datetime(df["Exposure_date"], format="%d/%m/%Y").dt.strftime("%A %d %B %Y")
+    df["data_timetext"] = df["Exposure_time"]
+    df["data_added"] = df.apply(lambda row: f"{row['Added_date']} {row['Added_time']}", axis=1)
+    df["data_added"] = pd.to_datetime(df["data_added"], format="%d/%m/%Y %H:%M:%S")
 
     df = df.drop(col_names, axis=1)
+    df = df.sort_values(by=["severity"])
 
     return df
